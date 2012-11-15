d441b623b7a2da72e89f27a737414bf79d210b65
Ismael Strada <isma_xxe@hotmail.com>
2012-11-14 22:06:33 -0200

    adicionado css

---
+++ site/page1.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Page 1</title>
+	<link rel="styllesheet" href="css/bootstrap.css" />;
+</head>
+<body>
+	
+	<h1>Título </h1>
+	<p>Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. 
+		Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, 
+		aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere 
+		pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat 
+		elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus 
+		scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur 
+		adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa.</p>
+</body>
+</html>
