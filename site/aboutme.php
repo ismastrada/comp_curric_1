8174801cc7970591f2e9b91abe66d984f0c1b6e9
Ismael Strada <isma_xxe@hotmail.com>
2012-11-16 19:25:30 -0200

    pagina aboutme.php

--- site/aboutme.php
+++ site/aboutme.php
@@ -1,10 +1,59 @@
-<!doctype html>
-<html lang="pt-br">
-<head>
-	<meta charset="UTF-8">
-	<title>Titulo</title>
-</head>
-<body>
-	
-</body>
+<!DOCTYP html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Sobre mim - Ismael Strada</title>
+	</head>
+
+	<body>
+		<h1>Sobre mim</h1>
+		<div class="Apresentacao" height="300px" width="220px">
+			<h2>Apresentação</h2>
+			<img src="https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-ash3/169053_101209579959637_6921075_n.jpg" align="left" alt="Foto do facebook" width="160px" height="160px" style="padding: 10px" />
+			<br />
+			<ul padding-left="20px" style="list-style-type: none">
+				<li>Nome: Ismael Strada</li>
+				<li>Profissão:  Ass. Administrativo – Prefeitura Mun. Xanxerê</li>
+				<li>Participo do movimento escoteiro desde 1998</li>
+				<li>Gosto de ouvir rock, ‘arranho’ uma guitarra e gaita de boca</li>
+				<li>Participo da Organização Internacional de Desenvolvimento Humano Integral <b>Cóndor Blanco</b> desde junho de 2011.</li>
+				<li>Curso Tecnologia em Análise e Desenvolvimento de Sistemas - 4º período</li>
+			</ul>
+		</div>
+		<br />
+		
+		<div class="Conhecimento">
+			<h2>Conhecimento</h2>
+			<ul>
+				<li>C</li>
+				<li>Java</li>
+				<li>HTML</li>
+				<li>CSS</li>
+				<li>SQL</li>
+				<li>Métodos ágeis de desenvolvimento - XP </li>
+				<li>Análise de requisitos / UML</li>
+				<li>Modelagem de banco de dados</li>
+			</ul>
+		</div>
+		<div class="Objetivos">
+			<h2>Objetivos</h2>
+			<p>Trabalhar com desenvolvimento de software na parte de back-end.</p>
+			<p>Expectativas para a disciplina Componente Curricular Complementar: Aprender a linguagem PHP e conseguir programar coisas simples ao final da disciplina.</p>
+		</div>
+		<div class="Contatos">
+			<h2>Contatos</h2>
+			<ul>
+				<li><a href="mailto:strada.isma@gmail.com">Gmail</a></li>
+				<li><a href="mailto:isma_xxe@hotmail.com">Hotmail/msn</a></li>
+				<li>Skype: isma_xxe</li>
+				<li><a href="http://www.facebook.com/IsmaelStrada" target="_blank" title="Visualizar perfil no Facebook">Facebook</a></li>
+				<li><a href="http://www.linkedin.com/profile/view?id=155240904&trk=tab_pro" target="_blank" title="Visualizar perfil no Linkedin">Linkedin</a></li>
+				<li><a href="http://www.slideshare.net/IsmaelStrada" target="_blank" title="Visualizar perfil no Slideshare">Slideshare</a></li>
+				<li>Celular: (49)9927-2702</li>
+				<li>Residencial: (49)3433-2219</li>
+				<li>Trabalho: (49)3433-7017</li>
+			</ul>
+		</div>
+	</body>
+
 </html>
